4f66f698365ce0e1e4ae8bc58585dee7c0d5ef15
SergeyKudinov <serezha.kudinov.1995@mail.ru>
2022-09-04 19:33:57 +0300

    almost end backend

--- server/index.js
+++ server/index.js
@@ -3,7 +3,9 @@ const sequelize = require('./db');
 const express = require('express');
 const models = require('./models/models');
 const cors = require('cors');
+const fileUpload = require('express-fileupload');
 const router = require('./routes/index');
+const errorHandler = require('./middleware/errorHandlingMiddleware');
 
 const PORT = process.env.PORT || 5000;
 
@@ -11,6 +13,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api', router);
+
+// Обработка ошибок, последний middleware
+app.use(errorHandler);
 
 const start = async () => {
     try {
